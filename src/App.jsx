9e2e797817ce972de0e31363d55446dcb9994549
Tokov <flyingcatsad@gmail.com>
2021-11-11 20:58:22 +0200

    Rename App js to jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter step={1}/>
+    </div>
+  );
+}
+
+export default App;
