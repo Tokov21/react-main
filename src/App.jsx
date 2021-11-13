f3c7ad829c09aed24300b8958a55c7ed1eeb3ffc
Tokov <flyingcatsad@gmail.com>
2021-11-13 22:24:25 +0200

    Add router to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-// import Counter from './components/Counter';
-// import Slider from "./components/Slider";
-import LoginForm from "./components/forms/LoginForm";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import MainPage from "./pages/MainPage";
+import SignupPage from "./pages/SignupPage";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Counter step={1}/> */}
-      {/* <Slider /> */}
-      <LoginForm />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route exact path="/register">
+          <SignupPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
