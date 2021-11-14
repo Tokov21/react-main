b015b3e8f8c67ee819fb958886ff907cfa361a97
Tokov <flyingcatsad@gmail.com>
2021-11-14 23:26:09 +0200

    Add onSubmit function

--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SignupForm from "../../components/Forms/SignupForm";
+import SignUpForm from "../../components/Forms/SignUpForm";
 
 export default function SignupPage() {
+  const onSubmit = (values, formikBag) => {
+    console.group();
+
+    console.log(values);
+    console.log(formikBag);
+
+    console.groupEnd();
+
+    formikBag.resetForm();
+  };
+
   return (
     <>
       <div>
         <Link to="/">Main</Link>
       </div>
-      <SignupForm />
+      <SignUpForm onSubmit={onSubmit} />
     </>
   );
 }
