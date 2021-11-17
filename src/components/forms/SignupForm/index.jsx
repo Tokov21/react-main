d7efe59a9feb42071ed80a75b2b9e8dd6ded8d3e
Tokov <flyingcatsad@gmail.com>
2021-11-17 12:28:01 +0200

    Add inputs to sign up form

--- src/components/forms/SignupForm/index.jsx
+++ src/components/forms/SignupForm/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Form, Formik } from "formik";
 
-import Input from "../Input";
+import Input from "../../Input";
+import Button from "../../Button";
 
 import { initialValues } from "../../../constants";
 import { SIGNUP_SCHEME } from "../../../validation/validationSchemes";
@@ -15,9 +16,18 @@ export default function SignUpForm({ onSubmit }) {
     >
       {() => (
         <Form>
-          <Input type="text" name="login" id="login" />
+          <Input type="text" name="firstname" id="firstname" />
+          <Input type="text" name="lastname" id="lastname" />
+          <Input type="text" name="displayname" id="displayname" />
+          <Input type="email" name="email" id="email" />
+          <Input type="password" name="password" id="password" />
+          <Input type="passwordConf" name="passwordConf" id="passwordConf" />
+          <Input type="radio" name="join" id="buyer" value="buyer" />
+          <Input type="radio" name="join" id="seller" value="seller" />
+          <Input type="checkbox" name="marketing" id="marketing" />
 
-          <button type="submit">Submit</button>
+          <Button type="submit" text="Create an account" />
+          {/* Google API */}
         </Form>
       )}
     </Formik>
