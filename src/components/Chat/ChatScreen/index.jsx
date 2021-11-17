597fa913e3c89ba2e0bb80b7dcf0bccd558b0b88
Tokov <flyingcatsad@gmail.com>
2021-11-17 14:24:10 +0200

    Remove console.log

--- src/components/Chat/ChatScreen/index.jsx
+++ src/components/Chat/ChatScreen/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function ChatScreen({ chatMessages }) {
-  console.log(chatMessages);
   const arrayJSX = chatMessages.map((msg, i) => (
     <li key={msg.id}>
       {msg.message} {new Date(msg.time).toString()}
