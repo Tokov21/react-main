544962716d1ca249aa227a904eaed793a43f07c2
Tokov <flyingcatsad@gmail.com>
2021-11-17 14:24:23 +0200

    Add placeholders

--- src/components/forms/SignupForm/index.jsx
+++ src/components/forms/SignupForm/index.jsx
@@ -15,16 +15,64 @@ export default function SignUpForm({ onSubmit }) {
       validationSchema={SIGNUP_SCHEME}
     >
       {() => (
-        <Form>
-          <Input type="text" name="firstname" id="firstname" />
-          <Input type="text" name="lastname" id="lastname" />
-          <Input type="text" name="displayname" id="displayname" />
-          <Input type="email" name="email" id="email" />
-          <Input type="password" name="password" id="password" />
-          <Input type="passwordConf" name="passwordConf" id="passwordConf" />
-          <Input type="radio" name="join" id="buyer" value="buyer" />
-          <Input type="radio" name="join" id="seller" value="seller" />
-          <Input type="checkbox" name="marketing" id="marketing" />
+        <Form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            maxWidth: "300px",
+          }}
+        >
+          <Input
+            type="text"
+            name="firstname"
+            id="firstname"
+            placeholder="First Name"
+          />
+          <Input
+            type="text"
+            name="lastname"
+            id="lastname"
+            placeholder="Last Name"
+          />
+          <Input
+            type="text"
+            name="displayname"
+            id="displayname"
+            placeholder="Nickname"
+          />
+          <Input type="email" name="email" id="email" placeholder="Email" />
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <Input
+            type="password"
+            name="passwordConf"
+            id="passwordConf"
+            placeholder="Confirm Password"
+          />
+          <Input
+            type="radio"
+            name="join"
+            id="buyer"
+            value="buyer"
+            textAfter="Join As a Buyer"
+          />
+          <Input
+            type="radio"
+            name="join"
+            id="seller"
+            value="seller"
+            textAfter="Join As a Creative or Marketplace Seller"
+          />
+          <Input
+            type="checkbox"
+            name="marketing"
+            id="marketing"
+            textAfter="Allow Squadhelp to send marketing/promotional offers from time to time"
+          />
 
           <Button type="submit" text="Create an account" />
           {/* Google API */}
