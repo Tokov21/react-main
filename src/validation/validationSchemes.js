29701ce18084c7e3f95aabea1424cbde2160c6ab
Tokov <flyingcatsad@gmail.com>
2021-11-17 12:29:15 +0200

    Add validation scheme

--- src/validation/validationSchemes.js
+++ src/validation/validationSchemes.js
@@ -1,5 +1,12 @@
 import * as yup from "yup";
 
 export const SIGNUP_SCHEME = yup.object().shape({
-  login: yup.string().email().required(),
+  firstname: yup.string().matches().required(),
+  lastname: yup.string().matches().required(),
+  displayname: yup.string().matches().required(),
+  email: yup.string().email().required(),
+  password: yup.string().matches().required(),
+  /*  passwordConf: yup.string().matches().required(),
+  join: yup.string().matches().required(),
+  marketing: yup.string().matches().required(), */
 });
