d9b57e4437ebf0559d206accf3a696fd8d849f53
Tokov <flyingcatsad@gmail.com>
2021-11-22 00:02:11 +0200

    Change validation

--- src/validation/validationSchemes.js
+++ src/validation/validationSchemes.js
@@ -1,29 +1,70 @@
 import * as yup from "yup";
 
+import { regExp } from "common/regex";
+
+const {
+  firstname,
+  lastname,
+  displayname,
+  password: {
+    min5,
+    oneNumber,
+    engUpperCase,
+    engLowerCase,
+    oneSpecCh,
+    onlyRussian,
+  },
+} = regExp;
+
 export const SIGNUP_SCHEME = yup.object().shape({
   firstname: yup
     .string()
-    .matches(/^[A-Z]{1}[a-z]{2,15}$/)
-    .required(),
+    .matches(
+      firstname,
+      "Start from an uppercase letter"
+    )
+    .required("First name is required"),
   lastname: yup
     .string()
-    .matches(/^[A-Z]{1}[a-z]{2,15}$/)
-    .required(),
+    .matches(
+      lastname,
+      "Start from an uppercase letter"
+    )
+    .required("Last name is required"),
   displayname: yup
     .string()
-    .matches(/^[a-zA-Z][a-zA-Z0-9_-]{3,15}$/, "Must start from a letter")
-    .required(),
-  email: yup.string().email().required(),
+    .matches(
+      displayname,
+      "Start from a letter and min 4 characters"
+    )
+    .required("Display name is required"),
+  email: yup
+    .string()
+    .email("Must follow pattern: text@text.domain")
+    .required("Email is required"),
   password: yup
     .string()
-    .matches(
-      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
-      "Minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character"
-    )
-    .required(),
+    .test({
+      name: "test-password",
+      test: function (value) {
+        return onlyRussian.test(value)
+          ? this.createError({
+              message: `Only English letters allowed`,
+              path: "password",
+            })
+          : true;
+      },
+    })
+    .matches(min5, "Minimum 5 characters")
+    .matches(oneNumber, "At least 1 number")
+    .matches(engUpperCase, "At least one upper case letter")
+    .matches(engLowerCase, "At least one lower case letter")
+    .matches(oneSpecCh, "One special character: #?!@$ %^&*-")
+    .required("Password is required"),
   passwordConf: yup
     .string()
-    .oneOf([yup.ref("password"), null], "Passwords must match"),
-  join: yup.string().required(),
+    .oneOf([yup.ref("password")], "Passwords must match")
+    .required("Password confirmation is required"),
+  join: yup.string().required("Choose your account type: Buyer or Seller"),
   marketing: yup.boolean(),
 });
