4d2077e91c490feeb0c5b53960a5b0a8ce426b5c
Tokov <flyingcatsad@gmail.com>
2021-11-22 00:00:57 +0200

    Add props

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import { ErrorMessage, Field } from "formik";
+import classNames from "classnames";
 
-export default function Input({ id, name, textBefore, textAfter, ...props }) {
+import "./Input.sass";
+
+export default function Input({
+  className,
+  formikProps: { errors, touched },
+  id,
+  name,
+  textBefore,
+  textAfter,
+  ...props
+}) {
   return (
-    <label htmlFor={id}>
+    <label htmlFor={id} className={className}>
       {textBefore}
-      <Field name={name} id={id} {...props} />
-      <ErrorMessage name={name} />
+      <Field
+        name={name}
+        id={id}
+        {...props}
+        className={classNames("input", {
+          valid: !errors[name] && touched[name],
+          invalid: errors[name] && touched[name],
+        })}
+      />
+      <ErrorMessage
+        name={name}
+        render={(msg) => (
+          <div className={classNames("message", {
+            radioError: name === "join"
+          })}>
+            <span>*&nbsp;</span>
+            <p>{msg}</p>
+          </div>
+        )}
+      />
       {textAfter}
     </label>
   );
