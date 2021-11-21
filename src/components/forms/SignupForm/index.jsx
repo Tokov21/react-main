79443296fac4cdfcb6a80d3d5f2a4b1a58c76079
Tokov <flyingcatsad@gmail.com>
2021-11-21 23:59:45 +0200

    Add Inputs component

--- src/components/forms/SignupForm/index.jsx
+++ src/components/forms/SignupForm/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Form, Formik } from "formik";
 
-import Input from "../../Input";
 import Button from "../../Button";
+import Inputs from "./Inputs";
 
-import { initialValues } from "../../../constants";
-import { SIGNUP_SCHEME } from "../../../validation/validationSchemes";
+import { initialValues } from "constants.js";
+import { SIGNUP_SCHEME } from "validation/validationSchemes";
+import styles from "./SignUpForm.module.sass";
 
 export default function SignUpForm({ onSubmit }) {
   return (
@@ -14,67 +15,11 @@ export default function SignUpForm({ onSubmit }) {
       onSubmit={onSubmit}
       validationSchema={SIGNUP_SCHEME}
     >
-      {() => (
-        <Form
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            maxWidth: "300px",
-          }}
-        >
-          <Input
-            type="text"
-            name="firstname"
-            id="firstname"
-            placeholder="First Name"
-          />
-          <Input
-            type="text"
-            name="lastname"
-            id="lastname"
-            placeholder="Last Name"
-          />
-          <Input
-            type="text"
-            name="displayname"
-            id="displayname"
-            placeholder="Nickname"
-          />
-          <Input type="email" name="email" id="email" placeholder="Email" />
-          <Input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-          />
-          <Input
-            type="password"
-            name="passwordConf"
-            id="passwordConf"
-            placeholder="Confirm Password"
-          />
-          <Input
-            type="radio"
-            name="join"
-            id="buyer"
-            value="buyer"
-            textAfter="Join As a Buyer"
-          />
-          <Input
-            type="radio"
-            name="join"
-            id="seller"
-            value="seller"
-            textAfter="Join As a Creative or Marketplace Seller"
-          />
-          <Input
-            type="checkbox"
-            name="marketing"
-            id="marketing"
-            textAfter="Allow Squadhelp to send marketing/promotional offers from time to time"
-          />
+      {(props) => (
+        <Form className={styles.flex}>
+          <Inputs props={props}/>
 
-          <Button type="submit" text="Create an account" />
+          <Button type="submit" text="Create account" />
           {/* Google API */}
         </Form>
       )}
