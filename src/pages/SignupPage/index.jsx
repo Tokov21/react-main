722dc03405aad78c6205831c607088599767c3f8
Tokov <flyingcatsad@gmail.com>
2021-11-22 00:01:48 +0200

    Add class to wrapper div

--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import SignUpForm from "../../components/Forms/SignUpForm";
+
+import styles from "./SignUpPage.module.sass";
 
 export default function SignUpPage() {
   const onSubmit = (values, formikBag) => {
@@ -10,11 +13,11 @@ export default function SignUpPage() {
   };
 
   return (
-    <>
+    <div className={styles["sign-up-page"]}>
       <div>
         <Link to="/">Main</Link>
       </div>
       <SignUpForm onSubmit={onSubmit} />
-    </>
+    </div>
   );
 }
