ec3135bc9a484f17d73e326088f431b6a9d034ae
Tokov <flyingcatsad@gmail.com>
2021-11-22 15:04:19 +0200

    Rename regex to regexes

--- src/common/regexes.js
+++ src/common/regexes.js
@@ -1,4 +1,6 @@
 export const regExp = {
+  min3: /.{3,}/,
+  min4: /.{4,}/,
   firstname: /^[A-Z]{1}[a-z]{2,15}$/,
   lastname: /^[A-Z]{1}[a-z]{2,15}$/,
   displayname: /^[a-zA-Z][a-zA-Z0-9_-]{3,15}$/,
@@ -8,6 +10,7 @@ export const regExp = {
     engUpperCase: /(?=.*?[A-Z])/,
     engLowerCase: /(?=.*?[a-z])/,
     oneSpecCh: /(?=.*?[#?!@$ %^&*-])/,
-    onlyRussian: /[а-яА-Я]+/
+    onlyEnglish: /[а-яА-Я]+/,
   },
+  onlyLetters: /((?=.*)[#?!@$ %^&*-]+)|((?=.*)[0-9]+)|\W+/,
 };
